4599a251b030914336d0c5a107e4f03ca4ae00fa
Josimar Bazam <jbazam@MacBook-Pro-de-Josimar.local>
2024-10-10 16:56:54 -0300

    O Meu test Secure Code Convciso Vuln

--- CommandExecution/CommandExec-4.php
+++ CommandExecution/CommandExec-4.php
@@ -10,7 +10,7 @@
     </div>
     <div style="background-color:#c9c9c9;padding:20px;">
       <h1 align="center">Browse The Files!</h1>
-    <form align="center" action="CommandExec-4.php" method="$_GET">
+    <form align="center" action="CommandExec-4.php" method="GET">
       What's it:
       <input type="text" name="typeBox" value=""><br>
       <input type="submit" value="Submit">
@@ -20,32 +20,30 @@
     <?php
     if(!file_exists(".hidden")){
       mkdir(".hidden");
-      exec("echo \"flag:secret\" > .hidden/log4.txt");
+      file_put_contents(".hidden/log4.txt", "flag:secret");
       if(strtoupper(substr(PHP_OS, 0, 3)) === 'WIN'){
         exec("attrib +h .hidden");
       }
     }
     if(isset($_GET["typeBox"])){
-      $target =$_GET["typeBox"];
-      $substitutions = array(
-        '&&'=>'',
-        '& ' => '',
-        '&& ' => '',
-        ';'  => '',
-        '|' => '',
-        '-'  => '',
-        '$'  => '',
-        '('  => '',
-        ')'  => '',
-        '`'  => '',
-        '||' => ''
-      );
-      $target = str_replace(array_keys($substitutions),$substitutions,$target);
-      echo shell_exec($target);
-      if($_GET["typeBox"] == "secret")
+      $target = $_GET["typeBox"];
+      
+      // Whitelist of allowed commands
+      $allowedCommands = ['ls', 'dir', 'pwd'];
+      
+      if (in_array($target, $allowedCommands)) {
+        $output = [];
+        $returnCode = 0;
+        exec($target, $output, $returnCode);
+        echo implode("\n", $output);
+      } else {
+        echo "Invalid command.";
+      }
+      
+      if($target === "secret") {
         echo "You really found my secret!";
+      }
     }
-
     ?>
   </div>
   </body>
